a370adc6c17a578b9c7e1ce81be200c144256918
Lorenzo <lorenzo.baridotti@hotmail.com>
2022-07-26 00:49:52 -0300

    Exercícios para praticar A009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,6 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const tamanho = minhaString.length
+
+console.log(minhaString.trim())
+console.log(tamanho)
+console.log(minhaString.replaceAll('________', 'sticioso'))
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,6 @@
 //Crie a const para a frase aqui
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+const novaFrase = frase.replaceAll('verde','rosa')
+const novaFrase2 = novaFrase.replaceAll('azul','laranja')
+
+console.log(novaFrase2.replaceAll('mas não deixe o gato sair', 'MAS NÃO DEIXE O GATO SAIR'))
