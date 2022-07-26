ff64f3b59832b5d4ccfe700912239fa2212a9721
Lorenzo <lorenzo.baridotti@hotmail.com>
2022-07-26 20:10:46 -0300

    Exercícios para praticar A009

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,8 @@
 //Crie a const para a frase aqui
-const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS,  mas não deixe o gato sair"`
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
 const novaFrase = frase.replaceAll('verde','rosa')
 const novaFrase2 = novaFrase.replaceAll('azul','laranja')
-const extra = 'MAS NÃO DEIXE O GATO SAIR'.toUpperCase()
+const extra = 'mas não deixe o gato sair'.toUpperCase()
 
 console.log(novaFrase2.replaceAll('mas não deixe o gato sair', extra))
 console.log('A nova frase inclui a palavra verde?', novaFrase2.includes('verde'))
